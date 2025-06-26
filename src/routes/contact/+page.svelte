8c945348518935055cbcad6342c7024d43913a2b
murraycopps <murraycopps@gmail.com>
2025-06-25 23:20:57 -0400

    added new email contact to contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	//use the api keys for both email accounts
 	let data = {
 		access_key: '848fbb6e-fc42-4751-bc62-b0031d8359a4',
 		name: '',
@@ -7,12 +8,24 @@
 		message: '',
         form_name: 'Knife Store Contact Form'
 	};
+	const other_key = "7e0d51f0-8151-4612-ad56-954302bf0d6b";
+
 	let status = '';
 	const handleSubmit = async () => {
 		if (status == 'Submitting...') return;
 		status = 'Submitting...';
 		data.subject = 'Contact Form Submission Knife Store: ' + data.subject;
+		const other_data = {
+			access_key: other_key,
+			name: data.name,
+			email: data.email,
+			subject: data.subject,
+			message: data.message,
+			form_name: 'Knife Store Contact Form'
+		};
+
 		const json = JSON.stringify(data);
+		const other_json = JSON.stringify(other_data);
 
 		const response = await fetch('https://api.web3forms.com/submit', {
 			method: 'POST',
@@ -22,9 +35,20 @@
 			},
 			body: json
 		});
+		const other_response = await fetch('https://api.web3forms.com/submit', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: other_json
+		});
 
+		
 		const result = await response.json();
-		if (result.success) {
+		const other_result = await other_response.json();
+
+		if (result.success || other_result.success) {
 			status = result.message || 'Success';
 			data = {
 				access_key: '848fbb6e-fc42-4751-bc62-b0031d8359a4',
@@ -34,7 +58,7 @@
 				message: ''
 			};
 		} else {
-			console.log(result);
+			console.log(result, other_result);
 			status = result.message || 'Error';
 		}
 	};
@@ -47,11 +71,10 @@
 		<div>
 			<h2 class="text-3xl text-center mb-4 text-oglala">Contact me</h2>
 			<p class="text-2xl text-white">
-				If you would like to get in touch with me, please fill out the form and I will get back to
-				you as soon as possible. Please note that I am not currently accepting custom orders at this
+				If you would like to get in touch with me, please fill out the form or email me at circledancetools@gmail.com and I will get back to you as soon as possible. Please note that I am not currently accepting custom orders at this
 				time but feel free to reach out with your ideas and I will see what I can do.
 				<br /> <br />
-				Purchases are done through etsy but feel free to reach out with any questions you may have regarding
+				Purchases are generally done through etsy but feel free to reach out with any questions you may have regarding
 				my knives.
 				<br /> <br />
 				Thanks for stopping by!
