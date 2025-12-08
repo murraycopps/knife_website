9d8314c84e29fedff758ebf09515ddf60cce630a
murraycopps <murraycopps@gmail.com>
2025-12-08 11:33:57 -0500

    reworded

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -63,26 +63,25 @@
 		<CircleDanceIcon size={32} class_name="text-white " fill="#a50021" bold={true} />
 	</div>
 	<p class="text-xl enter text-l text-white col-span-2 lg:col-span-3">
-		When I was ten, I was given another name. At Night Eagle, a summer camp deep in the woods, I
-		received the name Circle Dance—a name that reflected who I was and the qualities others saw in
-		me. This camp, where carving, bushcraft, and survival weren’t just pastimes but everyday
-		practices, became a formative part of my life. It taught me skills, values, and a deep
-		appreciation for tools that work hard without compromise. Frustrated by the knives I had back
-		then, I began making my own—to do better with less.
+		When I was ten, I was given another name. At Night Eagle, a summer camp in the woods of Vermont, I
+		received the name Circle Dance, a name shaped by the qualities others saw in
+		me using Lakota definitions. This camp, where carving, bushcraft, and survival skills were everyday activities, became a formative part of my life. It taught me skills, values, and a deep
+		appreciation for simple tools that do hard work without compromise. Frustrated by the knives I had back
+		then, I began making my own, trying to make them correctly by improving the simplist, most basic parts first.
 		<br /><br />
-		My philosophy is simple: make high-quality knives that can take a beating, stay sharp, and feel right
-		in your hand. I lean toward traditional blade shapes and handle styles, often inspired by bushcraft,
-		puukko, and sloyd knives—hard users built to be compact, comfortable, and capable of serious work,
+		My goal is simple: make high-quality knives that can take a beating, stay sharp, and feel right
+		in your hand. I lean toward traditional blade shapes and handle styles, and am often inspired by bushcraft,
+		puukko, and sloyd knives. They are hard users built to be compact, comfortable, and capable of serious work,
 		or carving knives refined for high-level performance. As the saying goes, “the smaller the knife,
-		the better the woodsman.” I aim to squeeze maximum capability out of every inch.
+		the better the woodsman.” I aim to squeeze maximum capability out of every inch and make knives meant for skilled users.
 		<br /><br />
 		Each blade is shaped through stock removal, finished by hand, and rigorously tested through real
 		use—especially during carving, where subtle flaws can’t hide. I prefer tough stainless steels like
 		14C28N and AEB-L for their reliability and value, while also exploring high-performance super steels
-		like CPM M4. Handles are made from wood or micarta, and all blades are heat-treated professionally
+		like CPM M4. Normally I avoid most simple carbon steels due to poor properity balance however some are great high toughness steels and Pops Procut is a new steel with great properties which is easy to work with. My handles are made from wood or micarta, and all blades, aside from my Procut knives, are heat-treated professionally
 		to precise specifications for optimal durability and edge retention.
 		<br /><br />
-		Circle Dance isn’t just a name—it’s a part of me. A reminder of where I started, and the mindset
+		Circle Dance isn’t just a name, it’s a part of me. A reminder of where I started, and the mindset
 		that guides every tool I make.
 	</p>
 </div>
