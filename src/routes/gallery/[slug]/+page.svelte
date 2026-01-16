5d7f2257c938aff3fbbe35a9e41fea97bf81a0f9
murraycopps <murraycopps@gmail.com>
2026-01-16 10:34:22 -0500

    fixed slug checking

--- src/routes/gallery/[slug]/+page.svelte
+++ src/routes/gallery/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Data from '$lib/scripts/Data.js';
 	import { onMount } from 'svelte';
-	export let slug;
+	export let data;
 
 	let knife = null;
 	let numberOfImages = 0;
@@ -9,9 +9,10 @@
 	let gridClass = 'grid-cols-5';
 	let displayedImages = [];
 	let displayedOffset = 0;
+	let knifeExists = true;
 	async function fetchData() {
-		const data = await Data.getGallery();
-		knife = data.find((item) => item.id === slug);
+		const knives = await Data.getGallery();
+		knife = knives.find((item) => item._id === data.slug);
 		if (knife) {
 			numberOfImages = knife.images.length;
 			switch (numberOfImages) {
@@ -34,6 +35,8 @@
 					updateDisplayedImages(2, 2);
 					displayedOffset = 2;
 			}
+		} else {
+			knifeExists = false;
 		}
 	}
 
@@ -65,8 +68,8 @@
 </svelte:head>
 {#if knife}
 	<div class=" flex flex-col items-center gap-4 justify-center p-16">
-		<h1 class="text-4xl font-bold  text-center">{knife.name}</h1>
-		<p class="text-lg ">{knife.description}</p>
+		<h1 class="text-4xl font-bold text-center">{knife.name}</h1>
+		<p class="text-lg">{knife.description}</p>
 		<div class="relative max-w-96 w-full text-black">
 			{#each knife.images as image, i}
 				<img
@@ -88,7 +91,7 @@
 			{/if}
 		</div>
 		{#if numberOfImages > 1}
-			<div class="grid gap-1 max-w-lg  text-white {gridClass}">
+			<div class="grid gap-1 max-w-lg text-white {gridClass}">
 				{#each displayedImages as img, i}
 					<button
 						on:click={() => {
@@ -107,8 +110,13 @@
 		{/if}
 	</div>
 {:else}
-	<div class="max-w-4xl mx-auto p-4">
-		<h1 class="text-4xl font-bold mb-4 text-center">Loading...</h1>
+	<div class="text-center mx-auto p-4">
+		{#if !knifeExists}
+			<h1 class="text-4xl font-bold mb-4">Knife Not Found</h1>
+			<a href="/gallery" class="text-oglala underline">Return to Gallery</a>
+		{:else}
+			<h1 class="text-4xl font-bold mb-4">Loading...</h1>
+		{/if}
 	</div>
 {/if}
 
